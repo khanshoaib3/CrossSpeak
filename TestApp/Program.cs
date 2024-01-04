c32a1a6bae4226b4c3f4d29ea9b113841a0bc427
Mohammad Shoaib Khan <shoaib.khan20@outlook.com>
2024-01-05 00:41:09 +0530

    Add test project

---
+++ TestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using DavyKager;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        CrossSpeak.MainClass.Instance.Initialize();
+        CrossSpeak.MainClass.Instance.Say("Hello There!");
+        CrossSpeak.MainClass.Instance.Close();
+    }
+}
